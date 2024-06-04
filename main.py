0b11be129aad3b5548d75739c38be24844a76be2
misrax <fordax05@gmail.com>
2024-06-04 03:51:04 +0300

    fix(swagger): fixing login URL in swagger

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
 
 
-bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+bearer_transport = BearerTransport(tokenUrl="user/login")
 
 auth_backend = AuthenticationBackend(
     name="jwt",
