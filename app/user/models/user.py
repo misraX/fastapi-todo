6e80eb258c5f1a772e0f202e379ce68d2121e9a5
misrax <fordax05@gmail.com>
2024-06-04 03:42:47 +0300

    feat(usre): Adding login by username and password

--- app/user/models/user.py
+++ app/user/models/user.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from fastapi import Depends
 from fastapi import Request
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_users import BaseUserManager, UUIDIDMixin
 from fastapi_users import exceptions, models, schemas
 from fastapi_users.models import UP
@@ -97,3 +98,32 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
             return created_user
         except IntegrityError:
             raise exceptions.UserAlreadyExists()
+
+    async def authenticate(
+        self, credentials: OAuth2PasswordRequestForm
+    ) -> Optional[models.UP]:
+        """
+        Authenticate and return a user following an email and a password.
+
+        Will automatically upgrade password hash if necessary.
+
+        :param credentials: The user credentials.
+        """
+        try:
+            user = await self.get_by_username(credentials.username)
+        except exceptions.UserNotExists:
+            # Run the hasher to mitigate timing attack
+            # Inspired from Django: https://code.djangoproject.com/ticket/20760
+            self.password_helper.hash(credentials.password)
+            return None
+
+        verified, updated_password_hash = self.password_helper.verify_and_update(
+            credentials.password, user.hashed_password
+        )
+        if not verified:
+            return None
+        # Update password hash to a more robust one if needed
+        if updated_password_hash is not None:
+            await self.user_db.update(user, {"hashed_password": updated_password_hash})
+
+        return user
